1537b9e0f26f74e11308c4b489930465ea752015
ToamilHere <168946458+Toamil03@users.noreply.github.com>
2025-04-30 15:31:58 -0400

    Pro Ex. 6(A,B)

---
+++ InchesToFeet.java
@@ -0,0 +1,12 @@
+public class InchesToFeet {
+    public static void main(String[] args) {
+        final int INCHES_IN_FOOT = 12;
+        
+        int inches = 86; // You can change this value
+        int feet = inches / INCHES_IN_FOOT;
+        int remainingInches = inches % INCHES_IN_FOOT;
+
+        System.out.println(inches + " inches is equal to " +
+                           feet + " feet and " + remainingInches + " inches.");
+    }
+}
